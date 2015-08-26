c01e8c0c35b2aa18d50c8c831c9a51f44287d664
ali <jalalniya.ali@gmail.com>
2015-08-26 12:19:24 +0430

    salam
    bjhb

---
+++ config.php
@@ -0,0 +1,10 @@
+<?
+global $config;
+$config['db']['host'] = 'localhost';
+$config['db']['user'] = 'root';
+$config['db']['pass'] = '';
+$config['db']['name'] = 'ppc';
+
+$config['lang'] = 'fa';
+
+$config['salt'] = 'kjsghj.?>l;lj.,/koi675534.,jjgd';
